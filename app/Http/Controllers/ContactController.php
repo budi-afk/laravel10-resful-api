a95d9feb2996864b638e919860462f4280ca2de6
budi-afk <budiman1347@gmail.com>
2024-05-04 22:06:02 +0700

    search contact API

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactCreateRequest;
 use App\Http\Requests\ContactUpdateRequest;
+use App\Http\Resources\ContactCollection;
 use App\Http\Resources\ContactResource;
 use App\Models\Contact;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -63,7 +65,7 @@ class ContactController extends Controller
 
         $data = $request->validated();
 
-        $contact ->update($data);
+        $contact->update($data);
 
         return new ContactResource($contact);
     }
@@ -89,5 +91,39 @@ class ContactController extends Controller
         return response()->json([
             "message" => "contact deleted"
         ])->setStatusCode(200);
+    }
+
+    public function search(Request $request)
+    {
+        $user = Auth::user();
+        $page = $request->input("page", 1);
+        $size = $request->input("size", 10);
+
+        $contacts = Contact::query()->where("user_id", $user->id);
+
+        $contacts = $contacts->where(function (Builder $builder) use ($request) {
+
+            $name = $request->input("name");
+            if ($name) {
+                $builder->where(function (Builder $builder) use ($name) {
+                    $builder->orWhere("first_name", "like", "%$name%");
+                    $builder->orWhere("last_name", "like", "%$name%");
+                });
+            }
+
+            $email = $request->input("email");
+            if ($email) {
+                $builder->where("email", "like", "%$email%");
+            }
+
+            $phone = $request->input("phone");
+            if ($phone) {
+                $builder->where("phone", "like", "%$phone%");
+            }
+        });
+
+        $contacts = $contacts->paginate(perPage: $size, page: $page);
+
+        return new ContactCollection($contacts);
     }
 }
